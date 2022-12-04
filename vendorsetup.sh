76b7280ca8132a7517c55252db0e95cd3f19429a
Ruchit Marathe <ruchitmarathe@gmail.com>
2022-12-04 16:50:44 +0530

    a72q: Fix derp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -21,4 +21,4 @@ export OF_MAINTAINER="Risen"
 export FOX_BUILD_TYPE="Beta"
 export FOX_VERSION="R11.1_2"
 export OF_FL_PATH1="/tmp/flashlight" # See /init.recovery.qcom.rc for more information
-export OF_FL_PATH2="
+export OF_FL_PATH2=""
