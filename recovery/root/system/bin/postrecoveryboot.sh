ab1bfa9b9ec20b792b24ff8c44864cb576254f71
Simon1511 <me@simon1511.de>
2023-05-17 17:48:32 +0200

    a72q: disable recovery restoration on Android 11
    Change-Id: Ie620b0106b062ca1209813dfb1cbf112e4be39ce

--- recovery/root/system/bin/postrecoveryboot.sh
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -14,16 +14,25 @@
 #
 
 mkdir -p "/tmp/vendor";
+mkdir -p "/tmp/system";
 mount -w "/dev/block/mapper/vendor" "/tmp/vendor";
+mount -w "/dev/block/mapper/system" "/tmp/system";
 
+# Android 12+
 if [ -f "/tmp/vendor/recovery-from-boot.p" ]; then
   echo "I:postrecoveryboot: Removing stock recovery file in /vendor to prevent the stock ROM from replacing TWRP." >> /tmp/recovery.log;
   rm "/tmp/vendor/bin/install-recovery.sh";
   rm "/tmp/vendor/etc/init/vendor_flash_recovery.rc";
   rm "/tmp/vendor/recovery-from-boot.p";
+elif [ -f "/tmp/system/system/recovery-from-boot.p" ]; then
+  # Android 11
+  rm "/tmp/system/system/recovery-from-boot.p" ];
+  rm "/tmp/system/system/bin/install-recovery.sh" ];
 fi;
 
 umount "/tmp/vendor";
+umount "/tmp/system";
 rm -r "/tmp/vendor";
+rm -r "/tmp/system";
 
 exit 0;
